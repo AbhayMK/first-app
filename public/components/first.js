73485aae2b46617e9708ec6da043eb753dc93c4a
AbhayMK <abhay.physics@gmail.com>
2022-11-04 16:04:01 +0530

    sdsdsds

--- public/components/first.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const first = () => {
-  return <div>first</div>;
-};
-
-export default first;
--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,19 +1,7 @@
 import React from "react";
-import ReactDOM from 'react-dom';
-import Tilt from 'react-parallax-tilt';
-
 
 const Logo = () => {
-  return (
-    <div className='ma4 mt0'> 
-    <Tilt className='Tilt' options={{max:25}} style={{ height: '200px',width: '200px', backgroundColor: 'blue' }}>
-        <h2>React Tilt 👀</h2>
-      </div>
-    </Tilt>
-    </div>
-  );
+  return <div className="ma4 mt0"></div>;
 };
-
-//ReactDOM.render(<Logo />, document.getElementById('root'));
 
 export default Logo;
